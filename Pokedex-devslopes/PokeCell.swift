97a02959ecb3e81a8fa0b225d1070fd0c501e5fe
Shane McKenzie <shane.mckenzie59@gmail.com>
2016-08-21 16:59:07 -0600

    *Collection View Delegate

---
+++ Pokedex-devslopes/PokeCell.swift
@@ -0,0 +1,32 @@
+//
+//  PokeCell.swift
+//  Pokedex-devslopes
+//
+//  Created by Shane Mckenzie on 8/21/16.
+//  Copyright © 2016 Shane Mckenzie. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 9.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
